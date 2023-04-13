55d21ec17a42716462e49d90f4bdb128e22d169e
Factless <diterpro26@gmail.com>
2023-04-13 07:50:09 +0300

    first commit

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-def greet():
-    print("------------------")
-    print(" Приветствуем вас ")
-    print("      в игре      ")
-    print(" крестики-нолики  ")
-    print("------------------")
+class Dot:
+    def __init__(self, x, y):
+     self.x = x
+     self.y = y
+  #  def __eq__(self, other):
+a = Dot(1 , 1)
+b = Dot(3 , 2)
+c = Dot(1 , 1)
 
-greet()
+print(a == c)
